e06000680385e61a509ea2e95ddd86508621d1c7
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-05-06 20:25:41 -0300

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,5 +4,5 @@ st.write('# Classificação de Iris')
 st.write('## Exemplo com comprimento de petala e sepala.')
 
 st.sidebar.write('### Parametros')
-st.sidebar.slider('Comprimento da Sépala', 4, 8, 5.8, 0.1)
-st.sidebar.slider('Comprimento da Pétala', 0.9, 7, 3.8, 0.1)
+st.sidebar.slider('Comprimento da Sépala', 4.0, 8.0, 5.8, 0.1)
+st.sidebar.slider('Comprimento da Pétala', 0.9, 7.0, 3.8, 0.1)
