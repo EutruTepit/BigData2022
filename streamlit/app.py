9faa33c0c3b85fbb185b109c90177a72d0477335
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-05-06 20:59:16 -0300

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 
 st.write('# Classificação de Iris')
 st.write('## Exemplo com comprimento de petala e sepala.')
@@ -6,3 +9,6 @@ st.write('## Exemplo com comprimento de petala e sepala.')
 st.sidebar.write('### Parametros')
 st.sidebar.slider('Comprimento da Sépala', 4.0, 8.0, 5.8, 0.1)
 st.sidebar.slider('Comprimento da Pétala', 0.9, 7.0, 3.8, 0.1)
+
+with open('objetos.pkl') as arquivo:
+    ss, dtc = pickle.load(arquivo)
