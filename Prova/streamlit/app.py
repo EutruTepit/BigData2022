ec148bdbf8631d9d8f2299fbdd275a954c59bfaa
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-05-20 18:28:43 -0300

    teste de if

--- Prova/streamlit/app.py
+++ Prova/streamlit/app.py
@@ -89,7 +89,7 @@ df = ss.transform(df)
 st.write(df)
 
 predicao = dtc.predict(df)
-#predicao[0].map({0:'benigno', 1:'maligno'})
+predicao[0] = "Benigno" if (predicao[0] == 0) else "Maligno"
 st.write(f"O tipo de cancer predito é: **{predicao[0]}**")
 
 mapeamento = {
