ce9296d1db32f7bedd11c51db9a60bb303bdbd75
Welinton haas <46527501+welintonhaas@users.noreply.github.com>
2022-05-20 17:59:17 -0300

    Update app.py

--- Prova/streamlit/app.py
+++ Prova/streamlit/app.py
@@ -98,6 +98,8 @@ predicao_proba = dtc.predict_proba(df)
 predicao_proba = pd.DataFrame(predicao_proba)
 predicao_proba.rename(mapeamento, axis=1, inplace=True)
 
-st.write(f"O tipo de cancer predito é: **{predicao[0]}**")
+pred = predicao[0].map(mapeamento)
+
+st.write(f"O tipo de cancer predito é: **{pred}**")
 st.write(f"Probabilidade:")
 st.write(predicao_proba)
